1791441f166fdf56ce2f80fef0ccb9a0d2e2a093
Tamas Meleg <v-tmeleg@hotels.com>
2016-08-16 14:09:08 +0200

    use conversationData for state variables

--- index.js
+++ index.js
@@ -42,30 +42,31 @@ server.get('/privacy', restify.serveStatic({
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var chatBot       = new builder.UniversalBot(connector, {
-						persistConversationData: true
-					}),
-    queue         = [],
-    status        = 'free',
-    currentPlayer = '';
+var chatBot = new builder.UniversalBot(connector, { persistConversationData: true });
+
+const STATUS_FREE = 'free';
+const STATUS_OCCUPIED = 'occupied';
 
 function readQueue(conversationData) {
-	queue = conversationData.queue || [];
+	var queue = conversationData.queue || [];
 
 	if (queue.length > 0) {
-		status = 'occupied';
-		currentPlayer = queue[0];
+		conversationData.status = STATUS_OCCUPIED;
+		conversationData.currentPlayer = queue[0];
 	} else {
-		status = 'free';
+		conversationData.status = STATUS_FREE;
 	}
+
+	return queue;
 }
 
-function saveQueue(conversationData) {
+function saveQueue(conversationData, queue) {
 	conversationData.queue = queue;
 }
 
-function setPP(userName, sender) {
-	var queueIndex = queue.indexOf(userName),
+function setPP(userName, sender, conversationData) {
+	var queue      = readQueue(conversationData),
+		queueIndex = queue.indexOf(userName),
 	    name       = userName === sender ? 'You are' : `${userName} is`,
 	    response   = '',
 	    rest       = '',
@@ -77,26 +78,29 @@ function setPP(userName, sender) {
 		queue.push(userName);
 	}
 
-	status        = 'occupied';
-	currentPlayer = queue[0];
+	conversationData.status = STATUS_OCCUPIED;
+	conversationData.currentPlayer = queue[0];
 
-	if (currentPlayer === userName) {
+	if (conversationData.currentPlayer === userName) {
 		response = `${name} playing right now!`;
 	} else {
 		if (queue.length > 1) {
 			rest = `  \n\nWaiting: ` + queue.slice(1).join(', ');
 		}
 
-		nextPlayer = currentPlayer === sender ? `You` : currentPlayer;
+		nextPlayer = conversationData.currentPlayer === sender ? `You` : conversationData.currentPlayer;
 
 		response += `Current player: **${nextPlayer}**${rest}`;
 	}
 
+	saveQueue(conversationData, queue);
+
 	return response;
 }
 
-function setEOP(userName, sender) {
-	var queueIndex = queue.indexOf(userName),
+function setEOP(userName, sender, conversationData) {
+	var queue      = readQueue(conversationData),
+		queueIndex = queue.indexOf(userName),
 	    name       = userName === sender ? 'You are' : `${userName} is`,
 	    response   = '',
 	    rest       = '',
@@ -105,7 +109,7 @@ function setEOP(userName, sender) {
 	if (queueIndex !== -1) {
 		queue.splice(queueIndex, 1);
 
-		if (userName === currentPlayer) {
+		if (userName === conversationData.currentPlayer) {
 			response = `${userName} has finished the game.`
 		} else {
 			response = `${name} removed from the list.`
@@ -116,14 +120,14 @@ function setEOP(userName, sender) {
 	}
 
 	if (queue.length < 1) {
-		status = 'free';
+		conversationData.status = STATUS_FREE;
 
-		response += `\n\nNobody is playing. The table is **${status}**`;
+		response += `\n\nNobody is playing. The table is **${conversationData.status}**`;
 
 	} else {
-		currentPlayer = queue[0];
+		conversationData.currentPlayer = queue[0];
 
-		nextPlayer = currentPlayer === sender ? `You` : currentPlayer;
+		nextPlayer = conversationData.currentPlayer === sender ? `You` : conversationData.currentPlayer;
 
 		if (queue.length > 1) {
 			rest = `  \n\nWaiting: ` + queue.slice(1).join(', ');
@@ -131,6 +135,8 @@ function setEOP(userName, sender) {
 
 		response += `\n\nCurrent player: **${nextPlayer}**${rest}`;
 	}
+
+	saveQueue(conversationData, queue);
 
 	return response;
 }
@@ -151,12 +157,13 @@ _[username] - optional, if you want to manage other users in the queue_
 `;
 }
 
-function getStatus(sender) {
-	var response,
-	    name = currentPlayer === sender ? 'You' : currentPlayer,
-	    rest = '';
+function getStatus(sender, conversationData) {
+	var queue = readQueue(conversationData),
+	    name = conversationData.currentPlayer === sender ? 'You' : conversationData.currentPlayer,
+	    rest = '',
+	    response;
 
-	if (status === 'free') {
+	if (conversationData.status === STATUS_FREE) {
 
 		response = `Table is **free**.`;
 
@@ -172,10 +179,10 @@ function getStatus(sender) {
 	return response;
 }
 
-function clearQueue(command, parameter) {
+function clearQueue(command, parameter, conversationData) {
 	if (process.env.codeword && parameter === process.env.codeword) {
-		status = 'free';
-		queue  = [];
+		conversationData.status = STATUS_FREE;
+		saveQueue(conversationData, []);
 
 		return `Queue is cleared.`;
 	}
@@ -190,9 +197,7 @@ function generalReply(msg) {
 function getReply(session) {
 	var message          = session.message,
 		conversationData = session.conversationData,
-		conversation     = message.address.conversation.name,
-	    isGroup          = message.address.conversation.isGroup,
-	    botName          = message.address.bot.name,
+		botName          = message.address.bot.name,
 	    sender           = message.user.name,
 	    stripBotName     = new RegExp('^(?:\\s*@' + botName + '\\s+)?(\\S+)\\s*(.*)?', 'i'),
 	    textMatch        = message.text.trim().replace(/(<([^>]+)>)/ig, '').match(stripBotName),
@@ -201,36 +206,39 @@ function getReply(session) {
 	    userName         = parameter || sender,
 	    reply;
 
-	readQueue(conversationData);
-
 	switch (command) {
 		case 'pp'       :
-			reply = setPP(userName, sender);
+			reply = setPP(userName, sender, conversationData);
 			break;
 		case 'eop'      :
-			reply = setEOP(userName, sender);
+			reply = setEOP(userName, sender, conversationData);
 			break;
 		case 'help'     :
 			reply = getHelp(botName);
 			break;
 		case 's'        :
 		case 'status'   :
-			reply = getStatus(sender);
+			reply = getStatus(sender, conversationData);
 			break;
 		case 'clear' :
-			reply = clearQueue(command, parameter);
+			reply = clearQueue(command, parameter, conversationData);
 			break;
 		default         :
 			reply = generalReply(message.text);
 	}
 
-	saveQueue(conversationData);
-
 	return reply;
 }
 
 chatBot.dialog('/', function (session) {
-	var reply = getReply(session);
+	var isGroup = session.message.address.conversation.isGroup,
+		reply;
+
+	if (isGroup) {
+		reply = getReply(session);
+	} else {
+		reply = `Please use the group chat to communicate with me!`;
+	}
 
 	session.send(reply);
 });
