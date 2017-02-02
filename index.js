ab40115137ee66a79437ba026efa5cad9e454de3
Tamas Meleg <v-tmeleg@hotels.com>
2017-02-02 12:16:19 +0100

    Upgrade to botbuilder 3.5.4, accept edited messages

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 /* global process, require */
+'use strict';
 
-var fs      = require('fs');
-var restify = require('restify');
-var builder = require('botbuilder');
+const fs      = require('fs');
+const restify = require('restify');
+const builder = require('botbuilder');
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
 // Create chat bot
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId:       process.env.appId || null,
     appPassword: process.env.appSecret || null
 });
 
 // Setup Restify Server
-var server = restify.createServer(),
-    port   = process.env.port || process.env.PORT || 3978;
+const server = restify.createServer();
+const port   = process.env.port || process.env.PORT || 3978;
 
 server.listen(port, function () {
     console.log('%s listening to %s', server.name, server.url);
@@ -42,13 +43,13 @@ server.get('/privacy', restify.serveStatic({
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var chatBot = new builder.UniversalBot(connector, { persistConversationData: true });
-
+const chatBot = new builder.UniversalBot(connector, { persistConversationData: true });
+const confirmTimeout = process.env.confirmTimeout || 60;
 const STATUS_FREE = 'free';
 const STATUS_OCCUPIED = 'occupied';
 
 function readQueue(conversationData) {
-    var queue = conversationData.queue || [];
+    let queue = conversationData.queue || [];
 
     if (queue.length > 0) {
         conversationData.status = STATUS_OCCUPIED;
@@ -65,9 +66,11 @@ function saveQueue(conversationData, queue) {
 }
 
 function setPP(userName, sender, conversationData) {
-    var queue      = readQueue(conversationData),
+    let queue      = readQueue(conversationData),
         queueIndex = queue.indexOf(userName),
-        name       = userName === sender ? 'You are' : `${userName} is`,
+        senderName = sender.name,
+        senderId   = sender.id,
+        name       = userName === senderName ? 'You are' : `${userName} is`,
         response   = '',
         rest       = '',
         nextPlayer;
@@ -88,7 +91,7 @@ function setPP(userName, sender, conversationData) {
             rest = `  \n\nWaiting: ` + queue.slice(1).join(', ');
         }
 
-        nextPlayer = conversationData.currentPlayer === sender ? `You` : conversationData.currentPlayer;
+        nextPlayer = conversationData.currentPlayer === senderName ? `You` : conversationData.currentPlayer;
 
         response += `Current player: **${nextPlayer}**${rest}`;
     }
@@ -99,9 +102,11 @@ function setPP(userName, sender, conversationData) {
 }
 
 function setEOP(userName, sender, conversationData) {
-    var queue      = readQueue(conversationData),
+    let queue      = readQueue(conversationData),
         queueIndex = queue.indexOf(userName),
-        name       = userName === sender ? 'You are' : `${userName} is`,
+        senderName = sender.name,
+        senderId   = sender.id,
+        name       = userName === senderName ? 'You are' : `${userName} is`,
         response   = '',
         rest       = '',
         nextPlayer = '';
@@ -127,13 +132,17 @@ function setEOP(userName, sender, conversationData) {
     } else {
         conversationData.currentPlayer = queue[0];
 
-        nextPlayer = conversationData.currentPlayer === sender ? `You` : conversationData.currentPlayer;
+        nextPlayer = conversationData.currentPlayer === senderName ? `You` : conversationData.currentPlayer;
 
         if (queue.length > 1) {
             rest = `  \n\nWaiting: ` + queue.slice(1).join(', ');
         }
 
         response += `\n\nCurrent player: **${nextPlayer}**${rest}`;
+
+        // TODO: confirmation flow
+        //response += `\n\n**${nextPlayer}** please confirm your reservation with a **pp** again in ` + confirmTimeout + ` seconds!`;
+        //conversationData.timer = setTimeout(nextPlayer, confirmTimeout * 1000);
     }
 
     saveQueue(conversationData, queue);
@@ -148,8 +157,8 @@ function getHelp(botName) {
 
 **help**:    You are reading this :)  \n
 **status**:  Displays the current occupancy.  \n
-**pp [username]**:      Reserve the table / Subscribe to the wating queue.  \n
-**eop [username]**:     Release the table / Unsubscribe from the wating queue.  \n
+**pp [username]**:      Reserve the table / Subscribe to the waiting queue.  \n
+**eop [username]**:     Release the table / Unsubscribe from the waiting queue.  \n
 
 _[username] - optional, if you want to manage other users in the queue_
 
@@ -158,8 +167,9 @@ _[username] - optional, if you want to manage other users in the queue_
 }
 
 function getStatus(sender, conversationData) {
-    var queue = readQueue(conversationData),
-        name = conversationData.currentPlayer === sender ? 'You' : conversationData.currentPlayer,
+    let queue = readQueue(conversationData),
+        senderName = sender.name,
+        name = conversationData.currentPlayer === senderName ? 'You' : conversationData.currentPlayer,
         rest = '',
         response;
 
@@ -199,37 +209,36 @@ function nextPlayer(session) {
 }
 
 function getReply(session) {
-    var message          = session.message,
+    let message          = session.message,
         conversationData = session.conversationData,
         botName          = message.address.bot.name,
         botId            = message.address.bot.id,
-        sender           = message.user.name,
-        stripBotName     = new RegExp('^(?:\\s*@?\\(?(?:' + botName + '|' + botId +')\\)?\\s+)?(\\S+)\\s*(.*)?', 'i'),
+        sender           = message.user,
+        stripBotName     = new RegExp('^(?:.*@?\\(?(?:' + botName + '|' + botId +')\\)?\\s+)?(\\S+)\\s*(.*)?', 'i'),
         textMatch        = message.text.trim().replace(/(<([^>]+)>)/ig, '').match(stripBotName),
         command          = textMatch && textMatch[1] && textMatch[1].trim().toLowerCase(),
         parameter        = textMatch && textMatch[2] && textMatch[2].trim(),
-        userName         = parameter || sender || 'Anonymous',
+        userName         = parameter || sender.name || 'Anonymous',
         reply;
 
     switch (command) {
-        case 'pp'       :
+        case 'pp':
             reply = setPP(userName, sender, conversationData);
             break;
-        case 'eop'      :
+        case 'eop':
             reply = setEOP(userName, sender, conversationData);
-            //conversationData.timer = setTimeout(nextPlayer, 3000);
             break;
-        case 'help'     :
+        case 'help':
             reply = getHelp(botName);
             break;
-        case 's'        :
-        case 'status'   :
+        case 's':
+        case 'status':
             reply = getStatus(sender, conversationData);
             break;
-        case 'clear' :
+        case 'clear':
             reply = clearQueue(command, parameter, conversationData);
             break;
-        default         :
+        default:
             reply = generalReply(message.text);
     }
 
@@ -237,7 +246,7 @@ function getReply(session) {
 }
 
 chatBot.dialog('/', function (session) {
-    var isGroup = session.message.address.conversation.isGroup,
+    let isGroup = session.message.address.conversation.isGroup,
         reply;
 
     if (isGroup) {
@@ -250,7 +259,7 @@ chatBot.dialog('/', function (session) {
 });
 
 chatBot.on('conversationUpdate', function (convUpdate) {
-    var address      = convUpdate.address,
+    let address      = convUpdate.address,
         membersAdded = convUpdate.membersAdded,
         msg;
 
